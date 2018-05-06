d69f7965c6659caff87608f248cc71747fe3b238
Srikar Gattepalli <srikargattepalli11@gmail.com>
2018-05-06 16:38:03 -0700

    Added SearchView

--- app/src/main/java/com/example/srikar/freshmaps/MainActivity.java
+++ app/src/main/java/com/example/srikar/freshmaps/MainActivity.java
@@ -3,32 +3,54 @@ package com.example.srikar.freshmaps;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button in = findViewById(R.id.sendButton);
-        in.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EditText editTextName = findViewById(R.id.buildingInput);
-                String str;
-                if (editTextName.getText().toString().length() != 0) {
-                    str = editTextName.getText().toString();
-                } else {
-                    str = "NoWhere";
-                }
+        ListView byTeacher = (ListView) findViewById(R.id.listViewTeacher);
+        ArrayList<String> teacherNames = new ArrayList<String>();
+        teacherNames.addAll(Arrays.asList(getResources().getStringArray(R.array.teacherArray)));
 
-                Intent startIntent = new Intent(getApplicationContext(), Mapper.class);
-                startIntent.putExtra("room", str);
-                startActivity(startIntent);
+        adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, teacherNames);
+        byTeacher.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+        MenuItem item = menu.findItem(R.id.menuSearch);
+        SearchView searchView = (SearchView) item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+
+                return false;
             }
         });
+        return super.onCreateOptionsMenu(menu);
     }
 }
