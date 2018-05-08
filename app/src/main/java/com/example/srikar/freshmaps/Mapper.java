fcde2ff5d4cdc14ea52da7397a2ffedb9826009a
Srikar Gattepalli <srikargattepalli11@gmail.com>
2018-05-08 09:13:59 -0700

    ImageView

--- app/src/main/java/com/example/srikar/freshmaps/Mapper.java
+++ app/src/main/java/com/example/srikar/freshmaps/Mapper.java
@@ -1,11 +1,15 @@
 package com.example.srikar.freshmaps;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 
@@ -29,7 +33,12 @@ public class Mapper extends AppCompatActivity {
             }
         });
 
+        ImageButton mapButton = (ImageButton) findViewById(R.id.evhsMap);
+        mapButton.setOnTouchListener(new View.OnTouchListener() {
+            public boolean OnTouch(View v, MotionEvent event) {
 
+            }
+        });
 
     }
 }
