46dcdb9313e4d3adca47e533858aca62c173deb5
Srikar Gattepalli <srikargattepalli11@gmail.com>
2018-05-09 02:34:28 -0700

    More

--- app/src/main/java/com/example/srikar/freshmaps/MainActivity.java
+++ app/src/main/java/com/example/srikar/freshmaps/MainActivity.java
@@ -1,19 +1,37 @@
 package com.example.srikar.freshmaps;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button schedule = findViewById(R.id.bellSchedule);
         Button name = (Button) findViewById(R.id.searchByTeacher);
         Button room = (Button) findViewById(R.id.searchByRoom);
+        schedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent startIntent = new Intent(getApplicationContext(), bellSchedule.class);
+                startActivity(startIntent);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+            }
+        });
         room.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,4 +51,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 }
