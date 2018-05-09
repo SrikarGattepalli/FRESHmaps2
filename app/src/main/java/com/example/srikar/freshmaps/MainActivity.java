d139aaf0e1e6c0095762885c7a5b32212b7e37f2
kamathanirudh6090 <anirudhkamath01@gmail.com>
2018-05-09 08:52:06 -0700

    Slide In Animations

--- app/src/main/java/com/example/srikar/freshmaps/MainActivity.java
+++ app/src/main/java/com/example/srikar/freshmaps/MainActivity.java
@@ -55,11 +55,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
 
-        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
-    }
 
 }
