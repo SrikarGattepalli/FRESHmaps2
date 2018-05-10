e8ff47108f1ee0f92a3e5cdec309c16ed6bc9940
Srikar Gattepalli <srikargattepalli11@gmail.com>
2018-05-09 22:00:07 -0700

    Home Screen and Calibrated to Samsung Phone

--- app/src/main/java/com/example/srikar/freshmaps/Tester.java
+++ app/src/main/java/com/example/srikar/freshmaps/Tester.java
@@ -1,35 +1,42 @@
 package com.example.srikar.freshmaps;
 
+import android.support.v7.app.AppCompatActivity;
+
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 /**
  * Class to test the java classes
  */
 public class Tester {
-    public static void main(String[] args){
-        try{
-            School test = new School("TestFile.txt");
-            for(Room r : test.getTotalSchool()){
-                System.out.println(r.getTeacher()+"    "+r.getRoomNumber());
+   /* public static void main(String[] args) {
+        try {
+            School test = new School(new InputStream() {
+                @Override
+                public int read() throws IOException {
+                    return 0;
+                }
+            };
+            for (Room r : test.getTotalSchool()) {
+                System.out.println(r.getTeacher() + "    " + r.getRoomNumber());
             }
             System.out.println();
             test.sortArrayByRoom();
-            for(Room r : test.getTotalSchool()){
-                System.out.println(r.getTeacher()+"    "+r.getRoomNumber());
+            for (Room r : test.getTotalSchool()) {
+                System.out.println(r.getTeacher() + "    " + r.getRoomNumber());
             }
             System.out.println();
             test.sortArraybyTeacher();
-            for(Room r : test.getTotalSchool()){
-                System.out.println(r.getTeacher()+"    "+r.getRoomNumber());
+            for (Room r : test.getTotalSchool()) {
+                System.out.println(r.getTeacher() + "    " + r.getRoomNumber());
             }
-        }
-        catch(Exception e){
-        System.out.println("woops");
+        } catch (Exception e) {
+            System.out.println("woops");
         }
 
 
-    }
+    }*/
 }
